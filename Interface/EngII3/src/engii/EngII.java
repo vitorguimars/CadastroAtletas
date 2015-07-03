e5a81a82814eba94abc5ead5aa0139e8abc1338d
unknown <631410044@SALA704-09.fspoaeduc.com.br>
2015-07-03 11:06:06 -0300

    atualizando interface

---
+++ Interface/EngII3/src/engii/EngII.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package engii;
+
+import Telas.*;
+
+/**
+ *
+ * @author 631110067
+ */
+public class EngII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Telainicial tela = new Telainicial();
+        tela.setVisible(true);
+    }
+    
+}
